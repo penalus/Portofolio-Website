05565b50956ad0287f3cdef4176b2d34ce8ba813
lotte olsen <olsenpenalus@gmail.com>
2023-04-17 07:58:07 +0200

    Add new page called Message Generator in the Projects dropdown menu and implement the random message generator into the website

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Clickable dropdown function in navigation menu
 function dropDown() {
     document.getElementById("dropDownContent").classList.toggle("show");
 }    
@@ -10,3 +11,34 @@ window.onclick = function(a) {
         }
     }
 }
+
+// Message Generator 
+const messagesObject = {
+    first: ["You", "Someone you know", "Your mother", "Your worst enemy"],
+    second: ["will", "will never", "will maybe", "will do everything they can to", "will kill to", "are secretly wishing to"],
+    third: ["be young", "be old", "be rich", "become president", "be poor", "have good fortune", "die very soon", "look young forever", "frame someone close to you for murder"]
+}
+
+
+function messageGenerator() {
+    let randomSentence = [];
+
+    const firstPiece = messagesObject.first;
+    const secondPiece = messagesObject.second;
+    const thirdPiece = messagesObject.third;
+
+    const firstRandomNumber = Math.floor(Math.random() * firstPiece.length);
+    const secondRandomNumber = Math.floor(Math.random() * secondPiece.length);
+    const thirdRandomNumber = Math.floor(Math.random() * thirdPiece.length);
+
+    randomSentence.push(firstPiece[firstRandomNumber]);
+    randomSentence.push(secondPiece[secondRandomNumber]);
+    randomSentence.push(thirdPiece[thirdRandomNumber]);
+
+    
+    document.getElementById("mg-message").innerHTML = randomSentence.join(' ');
+}
+
+messageGenerator();
+
+    
